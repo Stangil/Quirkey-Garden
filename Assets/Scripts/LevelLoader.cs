b22c0d17fc8240ca97ace79302b8c2db77d3e028
Stangil <stangil44@gmail.com>
2020-05-18 15:07:13 -0400

    Added splash and start screens

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] float delayBeforeSceneLoad = 3.0f;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreenAfterDelay());
+        }
+        else
+        {
+            LoadNextScene();
+        }
+    }
+
+    IEnumerator LoadStartScreenAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeSceneLoad);
+        LoadNextScene();
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
