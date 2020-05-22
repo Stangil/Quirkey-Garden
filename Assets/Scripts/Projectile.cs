beb28c827ff522ec7089083c82e1289bddfaa818
Stangil <stangil44@gmail.com>
2020-05-22 16:04:33 -0400

    Firing projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 1.0f;
+    [SerializeField] float spin = 1.0f;
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+}
