f060f8e6ea09753ec4ec5a8ad2d635ced2781622
Stangil <stangil44@gmail.com>
2020-05-26 11:41:10 -0400

    Added damage to attackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,6 +6,7 @@ public class Attacker : MonoBehaviour
 {
     [Range (0f,5f)]
     float currentSpeed = 0.0f;
+    [SerializeField] int health = 200;
   
     void Update()
     {
@@ -14,5 +15,30 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        Debug.Log("I'm Hit!");
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    private void EnemyDeath()
+    {
+        Destroy(gameObject);
     }
 }
