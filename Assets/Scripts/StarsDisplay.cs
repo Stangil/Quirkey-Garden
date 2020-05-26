930abdc2888b7b6dcfa464b59eef038d22f27bad
Stangil <stangil44@gmail.com>
2020-05-26 16:00:31 -0400

    Displaying stars

---
+++ Assets/Scripts/StarsDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StarsDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text starText;
+     void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+   private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+    public void AddStars(int starsToAdd)
+    {
+        stars += starsToAdd;
+        UpdateDisplay();
+    }
+    public void SpendStars(int starsToSpend)
+    {
+        if (stars >= starsToSpend)
+        {
+            stars -= starsToSpend;
+            UpdateDisplay();
+        }
+    }
+}
