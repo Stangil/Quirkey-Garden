896dc760c6a09ff30ea1970e1a8482cbfacd33fc
Stangil <stangil44@gmail.com>
2020-05-27 14:51:24 -0400

    Adding stars(resources)

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed = 1.0f;
-    [SerializeField] float spin = 1.0f;
+    [SerializeField] float spin = 100.0f;
     void Update()
     {
-        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime,Space.World);
+        transform.Rotate(Vector3.forward, spin * Time.deltaTime);
     }
 }
