925b5b671aab5867ac4c2b84ee072cb07b722a27
Stangil <stangil44@gmail.com>
2020-05-27 12:51:29 -0400

    Reorganized characters

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
     public void Fire()
     {
-        Instantiate(projectile, transform.Find("Gun").position, transform.rotation);
+        Instantiate(projectile, gun.transform.position, transform.rotation);
     }
 }
 
