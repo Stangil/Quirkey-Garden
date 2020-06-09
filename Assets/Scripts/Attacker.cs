fc8a35a377af35d4f0aaf7be38803914ed8d1403
Stangil <stangil44@gmail.com>
2020-06-09 15:31:54 -0400

    Created a health component and ablility for attackers to do damage

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +7,20 @@ public class Attacker : MonoBehaviour
 {
     [Range (0f,5f)]
     float currentSpeed = 0.0f;
-    [SerializeField] int health = 200;
-    [Header("Effects")]
-    [SerializeField] GameObject explosionParticles;
-    [SerializeField] float durationOfExplosion = 1.0f;
     GameObject currentTarget;
 
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
     }
     public void SetMovementSpeed(float speed)
     {
@@ -25,39 +31,17 @@ public class Attacker : MonoBehaviour
         GetComponent<Animator>().SetBool("IsAttacking", true);
         currentTarget = target;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void StrikeCurrentTarget(int damage)
     {
-        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
-        if (!damageDealer)
+        if (!currentTarget)
         {
             return;
         }
-        ProcessHit(damageDealer);
-    }
-
-    private void ProcessHit(DamageDealer damageDealer)
-    {
-        health -= damageDealer.GetDamage();
-        damageDealer.Hit();
-        if (health <= 0)
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
         {
-            EnemyDeath();
+            health.DealDamage(damage);
         }
     }
-
-    private void EnemyDeath()
-    {
-        TriggerDeathVFX();
-        Destroy(gameObject);
-    }
-
-    private void TriggerDeathVFX()
-    {
-        if (!explosionParticles)
-        {
-            return;
-        }
-        GameObject explosion = Instantiate(explosionParticles, transform.position, Quaternion.identity) as GameObject;
-        Destroy(explosion, durationOfExplosion);
-    }
+   
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 200;
+    [Header("Effects")]
+    [SerializeField] GameObject explosionParticles;
+    [SerializeField] float durationOfExplosion = 1.0f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    private void EnemyDeath()
+    {
+        TriggerDeathVFX();
+        Destroy(gameObject);
+    }
+
+    internal void DealDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!explosionParticles)
+        {
+            return;
+        }
+        GameObject explosion = Instantiate(explosionParticles, transform.position, Quaternion.identity) as GameObject;
+        Destroy(explosion, durationOfExplosion);
+    }
+}
